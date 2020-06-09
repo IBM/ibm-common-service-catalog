70e9509caf4b5001e4482e84514219af75fa7446
Chen Zhiwei <zhiweik@gmail.com>
2020-06-08 23:43:01 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ibm-common-service-catalog
-The Operator Catalog of IBM Common Services
+
+The Operator Catalog of IBM Common Services.
+
+When you want to deploy IBM Common Services, you need first create following OpenShift CatalogSource:
+
+```
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+  name: opencloud-operators
+  namespace: openshift-marketplace
+spec:
+  displayName: IBMCS Operators
+  publisher: IBM
+  sourceType: grpc
+  image: quay.io/opencloudio/ibm-common-service-catalog:latest
+```
---
+++ fetch-operator-bundles.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CS_OPERATORS=(
+    ibm-auditlogging-operator
+    ibm-catalog-ui-operator
+    ibm-cert-manager-operator
+    ibm-common-service-operator
+    ibm-commonui-operator
+    ibm-elastic-stack-operator
+    ibm-healthcheck-operator
+    ibm-helm-api-operator
+    ibm-helm-repo-operator
+    ibm-iam-operator
+    ibm-ingress-nginx-operator
+    ibm-licensing-operator
+    ibm-management-ingress-operator
+    ibm-metering-operator
+    ibm-mongodb-operator
+    ibm-monitoring-exporters-operator
+    ibm-monitoring-grafana-operator
+    ibm-monitoring-prometheus-operator-ext
+    ibm-platform-api-operator
+    operand-deployment-lifecycle-manager
+    )
+
+mkdir -p manifests
+
+for operator in ${CS_OPERATORS[@]}; do
+    curl -sLO https://github.com/IBM/$operator/archive/master.zip
+    unzip master.zip
+    \cp -r $operator-master/deploy/olm-catalog/* manifests/
+    \rm -rf master.zip $operator-master
+done
+
+\rm -f manifests/*.yaml
